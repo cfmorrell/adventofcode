481856b327aae5363526b41b1dab6f98eba8f99d
Chris Morrell <morrell@vt.edu>
2016-12-07 22:41:18 -0500

    Day 7

--- Day7/day7.py
+++ Day7/day7.py
@@ -5,27 +5,32 @@ import re
 
 filename = str(sys.argv[1])
 f = open(filename, 'r')
-newfile = open('out.txt','w')
+
 abbaPresent = re.compile(u'([a-z])(?!\\1)([a-z])\\2\\1')
 insideBrackets = re.compile(u'\\[[a-z]*([a-z])(?!\\1)([a-z])\\2\\1[a-z]*\\]')
-abaBefore = re.compile(u'([a-z])(?!\\1)([a-z])\\1.*\\[.*\\2\\1\\2.*\\]')
-abaAfter = re.compile(u'\\[.*([a-z])(?!\\1)([a-z])\\1.*\\].*\\2\\1\\2')
-aba = re.compile(u'(\\[[a-z]*([a-z])(?!\\2)([a-z])\\2[a-z]*\\].*\\3\\2\\3|([a-z])(?!\\4)([a-z])\\4.*\\[[a-z]*\\5\\4\\5[a-z]*\\])')
+#Part 2: cat input.txt | sed 's/\(.\)\1\{2,\}/\1 \1/g' | grep -Ec '(^|\])[a-z ]*(.)(.)\2.*\[[a-z ]*\3\2\3|\[[a-z ]*(.)(.)\4.*\][a-z ]*\5\4\5'
+
 
 sum = 0
 for line in f:
-    print line
+    print 'Start:',line.strip()
 #    present = abbaPresent.search(line)
 #    inside = insideBrackets.search(line)
 #    if present != None and inside == None:
 #        print 'TLS!!'
 #        sum += 1
-   # before = abaBefore.search(line)
-   # after = abaAfter.search(line)
-    result = aba.search(line)
-    if result:
-        newfile.write(line.strip()+'\n')
-        sum += 1
+    temp = re.sub(u'\\[[a-z]*\\]','.',line)
+    abaList = []
+    for j in range(0,2):
+        for i in re.finditer(u'([a-z])(?!\\1)[a-z]\\1',temp[j:]):
+            abaList.append(i.group(0))
+    for aba in abaList:
+        bab = aba[1]+aba[0]+aba[1]
+#        print aba,modmatched
+        if re.search('\\[[a-z]*' + bab + '[a-z]*\\]',line):
+            sum += 1
+            print 'Match:',aba, bab
+            break
 
 
 print 'Total:',sum
