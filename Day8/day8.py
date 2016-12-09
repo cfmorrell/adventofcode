7f75233beb554732d39430f9581ddcc0f6db0e23
Chris Morrell <chris.morrell@usma.edu>
2016-12-09 16:28:20 -0500

    Working on day 8

---
+++ Day8/day8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+filename = str(sys.argv[1])
+f = open(filename, 'r')
+
+def rotateX(column, distance):
+    print 'Rotate X'
+
+def rotateY(row, distance):
+    print 'Rotate Y'
+
+def addRect(width, height):
+    print 'Add Rect'
+
+def initScreen(width, height):
+    for x in range(
+
+screen = []
+
+
+for line in f:
+  print line
