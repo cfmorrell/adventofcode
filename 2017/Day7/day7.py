166124cf9538cd3d3b12801d85e04c754ec15953
Chris Morrell <chris.morrell@usma.edu>
2017-12-08 15:59:55 -0500

    Working on Day 7

--- 2017/Day7/day7.py
+++ 2017/Day7/day7.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python
 
+def buildtree(node):
+	tempweight = nodeweight[node] + sum([nodeweight[n] for n in nodes[node]])
+	if nodes[node] == []:
+		return {node:(nodeweight[node],tempweight)}
+	else:
+		return {node:(nodeweight[node],tempweight,[buildtree(i) for i in nodes[node]])}
+
+def checkbalance(node):
+	print node
+	values = [i.values()[0][1] for i in weightednodes[node][2] if len(weightednodes[node][2]) > 0]
+	print values
+	if weightednodes[node][2] == []:
+		return None
+	elif max(values) != min(values):
+		print 'unbalanced'
+		for n in weightednodes[node]:
+			print n
+		return nodes[node]
+	else:
+		return [checkbalance(n) for n in nodes[node]]
+
 import sys
 
 filename = str(sys.argv[1])
 f = open(filename, 'r')
 
+allnodes = []
 nodeweight = {}
+nodes = {}
 for line in f:
 	node = line.split()[0]
 	weight = int(line.split()[1][1:-1])
 	nodeweight[node] = weight
-
-f.seek(0)
-for line in f:
+	allnodes.append(node)
 	if '->' in line:
-		parent = line.split('->')[0].strip().split()[0]
-		children = [i.strip() for i in line.strip().split('->')[1].strip().split(',')]
-		firstweight = nodeweight[children[0]]
-		for child in children:
-			print nodeweight[child]
-			if nodeweight[child] != firstweight:
-				print parent,child,children
+		nodes[node] = [i.strip() for i in line.strip().split('->')[1].strip().split(',')]
+		allnodes.extend(nodes[node])
+	else:
+		nodes[node] = []
+
+for node in allnodes:
+	if allnodes.count(node) == 1:
+		rootnode = node
+
+weightednodes = buildtree(rootnode)
+
+# print [i.keys() for i in weightednodes[rootnode][2]]
+# print [i.values()[0][1] for i in weightednodes[rootnode][2]]
+
+print checkbalance(rootnode)
 
 
-# for node in allnodes:
-# 	if allnodes.count(node) == 1:
-# 		print node
