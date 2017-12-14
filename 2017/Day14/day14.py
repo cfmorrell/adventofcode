81ac1c4e46fc637de00119d4f016fa4aac46ed4c
Chris Morrell <chris.morrell@usma.edu>
2017-12-14 16:56:48 -0500

    Day 14 finished

--- 2017/Day14/day14.py
+++ 2017/Day14/day14.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 import sys
+sys.setrecursionlimit(10000)
 
 def knothash(plain):
     size = 256
@@ -39,17 +41,45 @@ def part1(f):
         c += b.count('1')
     return c
         
-def findregion(mem,loc,num):
-	mem[loc[1]][loc[0]] = num
-	if loc[1] > 0 and mem[loc[1]-1][loc[0]] == 1:
-		findregion(mem,(loc[1]-1,loc[0]),num)
-	if loc[1] < 127 and mem[loc[1]+1][loc[0]] == 1:
-		findregion(mem,(loc[1]+1,loc[0]),num)
-	if loc[1] > 0 and mem[loc[1]-1][loc[0]] == 1:
-		findregion(mem,(loc[1]-1,loc[0]),num)
-	if loc[1] > 0 and mem[loc[1]-1][loc[0]] == 1:
-		findregion(mem,(loc[1]-1,loc[0]),num)
+class Graph:
+ 
+    def __init__(self, row, col, g):
+        self.ROW = row
+        self.COL = col
+        self.graph = g
+ 
+    # A function to check if a given cell 
+    # (row, col) can be included in DFS
+    def isSafe(self, i, j, visited):
+        # row number is in range, column number
+        # is in range and value is 1 
+        # and not yet visited
+        return (i >= 0 and i < self.ROW and
+                j >= 0 and j < self.COL and
+                not visited[i][j] and self.graph[i][j])
+             
+ 
+    def DFS(self, i, j, visited):
+        rowNbr = [-1,  0, 0, 1];
+        colNbr = [ 0, -1, 1, 0];
+         
+        visited[i][j] = True
+ 
+        # Recur for all connected neighbours
+        for k in range(4):
+            if self.isSafe(i + rowNbr[k], j + colNbr[k], visited):
+                self.DFS(i + rowNbr[k], j + colNbr[k], visited)
 
+    def countIslands(self):
+        visited = [[False for j in range(self.COL)]for i in range(self.ROW)]
+
+        count = 0
+        for i in range(self.ROW):
+            for j in range(self.COL):
+                if visited[i][j] == False and self.graph[i][j] ==1:
+                    self.DFS(i, j, visited)
+                    count += 1
+        return count
 
 def part2(f):
     mem = []
@@ -58,12 +88,13 @@ def part2(f):
         h = knothash(string)
         b = []
         for i in h:
-            b.extend(list(bin(int(i,16))[2:].zfill(4)))
+            b.extend([int(i) for i in list(bin(int(i,16))[2:].zfill(4))])
         mem.append(b)
-    return mem
-
+    print(mem)
+    g = Graph(128,128,mem)
+    return g.countIslands()
 
 testkey = 'flqrgnkx'
 realkey = 'ffayrhll'
-#print(part1(realkey))
-print(part2(testkey))
+# print(part1(realkey))
+print(part2(realkey))
