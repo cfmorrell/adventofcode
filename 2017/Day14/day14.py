dcadfc42e26da2f17e2dee86acc45d0eb0fe8af9
Chris Morrell <chris.morrell@usma.edu>
2017-12-14 14:46:53 -0500

    Working on 14

--- 2017/Day14/day14.py
+++ 2017/Day14/day14.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys
 
 def knothash(plain):
     size = 256
     li = range(size)
     pos = skip = 0
-
+    plainlst = [ord(i) for i in plain] + [17, 31, 73, 47, 23]
     for _ in range(64):
-        for n in [ord(i) for i in plain]:
+        for n in plainlst:
             new_li = []
             for i in range(n):
                 new_li.append(li[(pos+i)%size])
@@ -29,19 +29,30 @@ def knothash(plain):
     return ''.join(shex)
 
 def part1(f):
-    h = knothash(f+'-0')
-    b = ''
-    for i in range(0,len(h),2):
-        b += bin(int(h[i:i+2],16))[2:]
-    print(b)
+    c = 0
+    for j in range(128):
+        string = f+'-'+str(j)
+        h = knothash(string)
+        b = ''
+        for i in h:
+            b += bin(int(i,16))[2:].zfill(4)
+        c += b.count('1')
+    return c
         
 
-def part2():
-    pass
+def part2(f):
+    mem = []
+    for j in range(128):
+        string = f+'-'+str(j)
+        h = knothash(string)
+        b = []
+        for i in h:
+            b.extend(list(bin(int(i,16))[2:].zfill(4)))
+        mem.append(b)
+    return mem
 
-filename = str(sys.argv[1])
-with open(filename) as f:
-    f = f.read().strip()
 
-print(part1(f))
-#print(part2())
+testkey = 'flqrgnkx'
+realkey = 'ffayrhll'
+#print(part1(realkey))
+print(part2(testkey))
