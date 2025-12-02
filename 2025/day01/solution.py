f3d6b719b9660b59def9845215818d717d880193
Chris Morrell <chris.morrell@westpoint.edu>
2025-12-01 19:30:27 -0500

    cleanup

--- 2025/day01/solution.py
+++ 2025/day01/solution.py
@@ -20,32 +20,24 @@ def part2():
     with open(filename) as f:
         turns = f.read().split()
         for turn in turns:
-            print("Turn",turn)
             direction = turn[0]
             distance = int(turn[1:])
             if direction == "L":
                 newnum = num - distance
             else:
                 newnum = num + distance
-            if newnum >= 0:
-                print("Started at",num,", ended at",newnum)
-            else:
-                print("Started at",num,", ended at",newnum%100,"-",newnum)
             if direction == "L" and newnum <= 0:
-                print("Passed Zero Left!!!")
                 passes = 1 + abs(newnum) // 100
                 if num == 0:
                     passes -= 1
-                print("Passed",passes,"times")
                 count += passes
             elif direction == "R" and newnum >= 100:
-                print("Passed Zero Right!!!")
                 passes = newnum // 100
                 count += passes
-                print("Passed",passes,"times")
 
-            print("Count: ",count)
             num = newnum % 100
+        print("Count: ",count)
+
 
 
 
